3a9f258fb09cf7cbba09d705f85e5464d435dc06
adairconlin <adair.conlin@gmail.com>
2021-11-29 14:41:15 -0500

    Add src folder with page-template file

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = profileDataArr => {
-    for(let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
+const [userName, github] = profileDataArgs;
 
-    console.log("================");
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs)
+fs.writeFile("index.html", generatePage(userName, github), err => {
+    if(err) throw err;
+    console.log("Portfolio completed! Checkout index.html to see the output!");
+});
