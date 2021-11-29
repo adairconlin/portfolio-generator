86eda92e7f2a76de33f1267ada046f16984839de
adairconlin <adair.conlin@gmail.com>
2021-11-29 15:33:35 -0500

    Add inquirer package and begin user prompt

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2);
-const [userName, github] = profileDataArgs;
+// const pageHTML = generatePage(userName, github);
 
 
-fs.writeFile("index.html", generatePage(userName, github), err => {
-    if(err) throw err;
-    console.log("Portfolio completed! Checkout index.html to see the output!");
-});
+// fs.writeFile("index.html", pageHTML, err => {
+//     if(err) throw err;
+//     console.log("Portfolio complete! Checkout index.html to see the output!");
+// });
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "userName",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
