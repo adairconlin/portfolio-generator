b272b753aea818cd10982185ca2002795f21f52e
adairconlin <adairconlin@gmail.com>
2021-11-30 12:44:26 -0500

    Final commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
-//?const { stubFalse } = require("lodash");
-const fs = require("fs");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const generatePage = require("./src/page-template.js");
 
 
@@ -132,22 +131,21 @@ const promptProject = portfolioData => {
     });
 };
 
-
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile("./dist/index.html", pageHTML, err => {
-            if(err) throw new Error(err);
-            console.log("Page created!");
-
-            fs.copyFile('./src/style.css', './dist/style.css', err => {
-                if(err) {
-                    console.log(err);
-                    return;
-                }
-                console.log("Style sheet successfully copied!");
-            })
-        });
-    });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    })
--- src/page-template.js
+++ src/page-template.js
@@ -69,7 +69,6 @@ const generateProjects = projectsArr => {
 
 module.exports = templateData => {
     const { projects, about, ...header } = templateData;
-    console.log(templateData);
 
     return `
 <!DOCTYPE html>
@@ -79,6 +78,7 @@ module.exports = templateData => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Portfolio Demo</title>
+        <link rel="stylesheet" href="style.css">
     </head>
 
     <body>
